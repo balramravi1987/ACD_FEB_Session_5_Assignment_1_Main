2ce2114ec3fd7a843246695555451b82d0e45fa2
balramravi1987 <balramravi1987@gmail.com>
2017-02-01 18:46:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var name = "Balramsingh" //assigning variable
+var age = 30 //assigning variable
+var dateofbirth = "11/03/1987" //assigning variable
+var placeofbirth = "Rajapalayam" //assigning variable
+console.log("Name: " +name +", Age: " +age +", Date of Birth: " +dateofbirth +", Place of birth: " +placeofbirth); //displays the data
